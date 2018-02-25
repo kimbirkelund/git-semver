d2a04fd8d1de72c1e69c1368483e6c8913daf181
Kim Birkelund <kim@birkelund.me>
2018-02-25 14:33:37 +0100

    Added test.

--- src/git-semver/Tests/Get-GitSemanticVersion.tests.ps1
+++ src/git-semver/Tests/Get-GitSemanticVersion.tests.ps1
@@ -2,4 +2,10 @@ Describe Get-GitSemanticVersion {
     It "Returns 1.0.0 when no version tags are present" {
         Get-GitSemanticVersion | Should Be "1.0.0"
     }
+
+    It "Returns version part of tag when on tag"{
+        CreateGitRepositoryAndGoToIt;
+        
+        safegit tag -a "release/1.2.3"
+    }
 }
